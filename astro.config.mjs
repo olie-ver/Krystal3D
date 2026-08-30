587e30c46189985bc3daafe33b42d32161b92b4d
Oliver Lie <oliverlie@krystal3d.com>
2026-08-29 20:15:38 -0700

    Updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig, envField, fontProviders } from 'astro/config';
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   fonts: [{
@@ -15,9 +17,15 @@ export default defineConfig({
       }]
     }
   }],
+
   env: {
     schema: {
       RESEND_API_KEY: envField.string({ context: "server", access: "secret" }),
     }
-  }
+  },
+
+  site: "https://www.krystal3d.com",
+  base: "/",
+  trailingSlash: "always",
+  adapter: vercel(),
 });
